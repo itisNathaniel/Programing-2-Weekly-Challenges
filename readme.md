0f9cd46ca630428f849e796a1a327ce162460003
Nathaniel Read <nathaniel@northfolk.co.uk>
2018-06-08 10:38:03 +0100

    Readme and Week 2 Challenge Added
    Also organised better

---
+++ readme.md
@@ -0,0 +1,18 @@
+﻿# Programming 2 (08134) - Additional Challenges
+
+For context, here are the challenges set from the slides:
+
+##### Week One - Primes
+- Prime number are considered to be like mathematical atoms in that they can be
+used to create any other number. In fact the fundamental theorem of arithmetic
+states that every whole number can be represented by a unique product of prime
+numbers. Write a method that take an in a positive integer and returns an array
+that contains the unique primes
+
+##### Week Two - Piglatin
+- Write a method that will convert an English sentence into Pig Latin
+- For a much biggerchallenge write a method that *tries* to convert Pig Latin back to English
+
+##### Week Three - Name Scoring
+- Read a text file and sort the names alphabetically from A-Z.
+- Then calculate the score for each name based upon the letters position in the alphabet and multiply the score by the position. Then add them all up.
---
+++ wk3-Name Scores/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace NameRank
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            // store ranks in dictionary
+            Dictionary<string, int> wordRank = new Dictionary<string, int>();
+
+            // dictionary letter scores
+            Dictionary<char, int> letterValue = new Dictionary<char, int>();
+            const string theAlphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            int position = 1;
+            foreach(char c in theAlphabet)
+            {
+                letterValue.Add(c, position);
+                position++;
+            }
+
+            // Import our names
+            List<string> names = new List<string>();
+            using (StreamReader wordList = new StreamReader("names.txt"))
+            {
+                while(true) 
+                {
+                    string thisLine = wordList.ReadLine();
+                    if (thisLine == null)
+                    {
+                        break;
+                    }
+                    names.Add(thisLine);
+                }
+
+            }
+
+            // Sort our names
+            names.Sort();
+
+            // look up names in alphabet 
+            int rankcount = 1;
+            foreach(string name in names)
+            {
+                int nameScore = 0;
+                foreach(char c in name)
+                {
+                    int letterscore = 0;
+                    letterValue.TryGetValue(c, out letterscore);
+                    nameScore += letterscore;
+                }
+
+                int NewNameScore = nameScore * rankcount;
+                wordRank.Add(name, NewNameScore);
+                rankcount++;
+            }
+
+            // print output and calculate cumulative
+            int nameScoreSoFar = 0;
+            foreach(string name in names) 
+            {
+                int score = 0;
+                wordRank.TryGetValue(name, out score);
+                nameScoreSoFar += score;
+                Console.WriteLine("{0} scored {1} - cumulative {2}", name, score, nameScoreSoFar);
+
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
